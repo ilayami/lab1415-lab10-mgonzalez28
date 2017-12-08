3f37937bfe78aea631cdbe696b60340eba8ac697
Miguel Gonzalez <mgonzalez28@luc.edu>
2017-12-08 17:44:16 +0000

    Added central state and neighbors

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,14 +1,28 @@
 package hw;
 
-import java.util.Arrays;
+import java.util.*;
+import org.jgrapht.Graph;
+import org.jgrapht.alg.color.GreedyColoring;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.ClosestFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
+import org.jgrapht.traverse.RandomWalkIterator;
 
 public class Main {
 
   public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+    final static String NE = "Nebraska";
+    final static String IA = "Iowa";
+    final static String CO = "Colorado";
+    final static String KS = "Kansas";
+    final static String MO = "Missouri";
+    final static String WY = "Wyoming";
+    final static String SD = "South Dakota";
+    
+    
+   
+    
   }
 }
