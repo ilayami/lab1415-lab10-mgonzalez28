fee05a664079f791b1512a062ac0fdbd1e63759b
Miguel Gonzalez <mgonzalez28@luc.edu>
2017-12-09 18:27:45 +0000

    add countryCodes

---
+++ src/main/java/hw/countryCodes.java
@@ -0,0 +1,11 @@
+package hw;
+
+public class countryCodes {
+  static final String NE = "Nebraska";
+  static final String IA = "Iowa";
+  static final String CO = "Colorado";
+  static final String KS = "Kansas";
+  static final String MO = "Missouri";
+  static final String WY = "Wyoming";
+  static final String SD = "South Dakota";
+}
